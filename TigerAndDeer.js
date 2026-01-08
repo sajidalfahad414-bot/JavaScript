ea5db2fc85a8aef52ac35ddf6e42cb288ea4fb47
Your Name <your.email@example.com>
2026-01-08 16:24:53 +0600

    TigerAndDeer

--- TigerAndDeer.js
+++ TigerAndDeer.js
@@ -5,10 +5,15 @@ function Animal(name,color,speed){
 }
 const Animal1 = new Animal("Tiger","Yellow",300);
 const Animal2 = new Animal("Deer","Black",600);
-if(Animal1.speed>Animal2.speed)
-{
-    console.log(Animal1.name + " is Fast");
+const Animal3 = new Animal("horse","Brown",200);
+function Detector(AnimalX,AnimalY){
+
+    if(AnimalX.speed>AnimalY.speed){
+        return AnimalX.name +" Is Faster";
+    }
+    else{
+        return AnimalY.name+ "Is Faster"
+    }
+    
 }
-else{
-    console.log(Animal2.name + " is Fast");
-}
+console.log(Detector(Animal1,Animal2));
