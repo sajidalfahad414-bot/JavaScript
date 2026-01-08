4c37811079612b2ab55530585f0523d0210d07f0
Your Name <your.email@example.com>
2026-01-08 17:01:44 +0600

    added animal detector logic

---
+++ AnimalUsingTernary.js
@@ -0,0 +1,17 @@
+function Animal(name, color, speed) {
+    this.name = name;
+    this.color = color;
+    this.speed = speed;
+}
+
+const Animal1 = new Animal("Tiger", "Yellow", 300);
+const Animal2 = new Animal("Deer", "Black", 600);
+const Animal3 = new Animal("Horse","Brown",800)
+function Detector(AnimalX, AnimalY) {
+    
+    return AnimalX.speed > AnimalY.speed 
+        ? AnimalX.name + " IS Faster" 
+        : AnimalY.name + " IS Faster";
+}
+
+console.log(Detector(Animal1, Animal3)); 
